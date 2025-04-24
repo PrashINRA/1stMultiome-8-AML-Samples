8b626bd622086679e2b1e2686f27b2e7a4c5cc3f
Prashant Singh <singh.prashant.iitkgp@gmail.com>
2025-04-24 11:20:09 +0200

    Update bcl_2_fastq.sh

--- bcl_2_fastq.sh
+++ bcl_2_fastq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This is the shell script to generate demultiplexed fastq files using bcl2 files using cellranger-arc
+#This is the shell script to generate demultiplexed fastq files using bcl files with cellranger-arc 2.0
 
 #SBATCH --job-name=cellranger_arc_mkfastq
 #SBATCH --output=cellranger_arc_mkfastq_%j.out
@@ -15,7 +15,7 @@
 #Load modules
 module load cellranger/arc-2.0.0
 
-export PATH=/trinity/home/psingh/usr/local/bin:$PATH
+export PATH=/trinity/home/psingh/usr/local/bin:$PATH #Require to invoke illumina's bcl2fastq 
 
 BCL_PATH="/net/beegfs/scratch/psingh/240514_VH00563_167_AACHTM5HV/" #Path to .bcl files
 SAMPLESHEET_PATH="/trinity/home/psingh/SampleSheets/atac.csv"  #Prepare the samplesheet 1st
